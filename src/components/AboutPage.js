35a5f2eb5ecf640a7bf3a7b751f3ab3856bd407a
aespinet <aelluvetaa@gmail.com>
2017-12-16 23:32:00 -0500

    Phase Two - fix 01

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -5,12 +5,6 @@ export default () => {
     <div>
       <p>Application created by Alan Espinet:</p>
       <p><a href="http://alanespinet.com/" target="_blank">alanespinet.com/</a></p>
-
-      <div>
-        <NavLink to="/about/bio">Bio</NavLink>
-        <NavLink to="/about/contact">Contact</NavLink>
-        <NavLink to="/about/references">References</NavLink>
-      </div>
     </div>
   );
 };
