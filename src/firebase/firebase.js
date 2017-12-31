ea73b586d2695ba0fb1218abad8a3c2940ad6fca
aespinet <aelluvetaa@gmail.com>
2017-12-31 16:29:08 -0500

    Phase Seven - complete

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,13 @@
 import * as firebase from 'firebase';
+// import devConfig from './config';
 
 const config = {
-  apiKey: "a",
-  authDomain: "b",
-  databaseURL: "https://urlsdbase.com",
-  projectId: "c",
-  storageBucket: "d",
-  messagingSenderId: "e"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
